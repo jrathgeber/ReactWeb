2403f6f28a4cbab5086b435f17748b6ec97afcfa
jrathgeber <jrathgeber@yahoo.com>
2020-11-21 08:20:01 -0500

    Very first change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
-import React from 'react';
-//import React, { Component } from 'react';
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const value = 'World I am Mechanizd';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mechanizd will be based on React!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-
-
 export default App;
-
