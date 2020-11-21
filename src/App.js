95805ff9d15f33e4e5d2cd4200622a0898cd9507
jrathgeber <jrathgeber@yahoo.com>
2020-11-20 23:11:06 -0500

    bit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
 
 function App() {
   const value = 'World I am Mechanizd';
   return <div>Hello {value}</div>;
 }
 
+
+
+
+class Button extends Component {
+  render() {
+    return <Button color="red" />;
+  }
+}
+
+
 export default App;
+
