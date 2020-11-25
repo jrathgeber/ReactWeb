d45309d087a5fba435db341212a4c5530015399f
jrathgeber <jrathgeber@yahoo.com>
2020-11-25 10:23:16 -0500

    yarn

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ShowAlert from './ShowAlert';
 import Button from './Button';
 
 
+//</div><div className="ag-theme-alpine" style={ { height: 400, width: 600 } }></div>
 
 function App() {
 
@@ -30,43 +31,49 @@ function App() {
 
   return (
 
-    /*
+
+<div id="parent">
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mechanizd will be based on React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br></br>
 
-         <button class="Button_Launch">Launch me</button> 
-         
-        <Button class="Button">Shoot </Button>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Mechanizd will be based on React!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <br></br>
 
-        <ShowAlert class="ShowAlert" />
+          <button class="Button_Launch">Launch me</button> 
+          
+          <Button class="Button">Shoot </Button>
 
-      </header>
+          <ShowAlert class="ShowAlert" />
+
+
+
+        </header>
+      </div>
+
+      <div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
+
+        <AgGridReact
+            rowData={rowData}>
+            <AgGridColumn field="make"></AgGridColumn>
+            <AgGridColumn field="model"></AgGridColumn>
+            <AgGridColumn field="price"></AgGridColumn>
+          </AgGridReact>
+
     </div>
 
-    */
-
-<div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
-<AgGridReact
-    rowData={rowData}>
-    <AgGridColumn field="make"></AgGridColumn>
-    <AgGridColumn field="model"></AgGridColumn>
-    <AgGridColumn field="price"></AgGridColumn>
-</AgGridReact>
 </div>
-
 
   );
 }
