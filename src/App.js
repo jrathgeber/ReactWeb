fcbc93eef55dc7d305b5df906497251f2f46e82d
jrathgeber <jrathgeber@yahoo.com>
2020-11-25 09:38:45 -0500

    ag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+
+//import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 import ShowAlert from './ShowAlert';
 import Button from './Button';
 
+
+
 function App() {
+
+
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+
+  const [rowData, setRowData] = useState([
+      {make: "Toyota", model: "Celica", price: 35000},
+      {make: "Ford", model: "Mondeo", price: 32000},
+      {make: "Porsche", model: "Boxter", price: 72000}
+  ]);
+
+
+
   return (
+
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -31,6 +55,19 @@ function App() {
 
       </header>
     </div>
+
+    */
+
+<div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
+<AgGridReact
+    rowData={rowData}>
+    <AgGridColumn field="make"></AgGridColumn>
+    <AgGridColumn field="model"></AgGridColumn>
+    <AgGridColumn field="price"></AgGridColumn>
+</AgGridReact>
+</div>
+
+
   );
 }
 
