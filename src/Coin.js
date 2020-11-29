292fc61e4c2861f9cd7fb21dc5d76def61c5c3c5
jrathgeber <jrathgeber@yahoo.com>
2020-11-29 08:42:18 -0500

    coin

--- src/Coin.js
+++ src/Coin.js
@@ -9,30 +9,51 @@ class App extends Component {
   }
 
   async componentDidMount() {
-  //  fetch(`https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`)
-  //    .then(res => res.json())
-  //    .then(json => this.setState({ data: json }));
+ //   fetch(`https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`)
+ //     .then(res => res.json())
+ //     .then(json => this.setState({ data: json }));
 
       const response = await fetch(`https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`);
       const json = await response.json();
-      this.setState({ data: json });
+      this.setState({ data: json.data });
+
+  console.log("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+  console.log("Response " +   {response});
+
+  console.log("json " + json);
+
+  console.log(this.state.data);
 
   }
+
+
+ // {this.state.data.map(el => (
+ //           
+ //   <li>
+ //     {el.name}: {el.id}
+ //   </li>
+ // ))}
+
 
   render() {
     return (
       <div>
-        <p>  {toString(this.state.data[0])} </p>
+
+
+
+        <p> Voila :  { JSON.stringify(this.state.data[0])} </p>
         <ul>
 
-         
 
-          {this.state.data.map(el => (
+        {this.state.data.map(el => (
             
-            <li>
-              {el.name}: {el.id}
-            </li>
-          ))}
+    <li key={el.id.toString()}>
+      {el.name}: {el.id}
+    </li>
+  ))}
+
+
         </ul>
       </div>
     );
